e44ef37c0e14e785594c021a2872f0c776c5e82b
Nikaido　Yuki <nikaido01101@gmail.com>
2025-05-23 13:43:24 +0900

    復習問題

--- fukuv0523/Form1.Designer.cs
+++ fukuv0523/Form1.Designer.cs
@@ -31,11 +31,13 @@
             components = new System.ComponentModel.Container();
             label1 = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaption;
             label1.Location = new Point(372, 201);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
@@ -48,11 +50,22 @@
             timer1.Enabled = true;
             timer1.Tick += timer1_Tick;
             // 
+            // button1
+            // 
+            button1.Location = new Point(566, 146);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -64,5 +77,6 @@
 
         private Label label1;
         private System.Windows.Forms.Timer timer1;
+        private Button button1;
     }
 }
--- fukuv0523/Form1.cs
+++ fukuv0523/Form1.cs
@@ -10,12 +10,19 @@ namespace fukuv0523
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Left += 10;
+            button1.Height += 100;
+            button1.Width += 100;
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
             timer1.Enabled = false;
             label1.Text = "nikadio";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
         }
     }
 }
